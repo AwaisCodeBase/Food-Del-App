12103ac420cf9c587c2fd1e8f30c41b317353cc5
shera <shera@sheras-MacBook-Pro.local>
2025-08-27 23:54:51 +0500

    Added cart and order controllers, middleware, and updated frontend components

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,12 +2,35 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import validator from "validator";
-import { response } from "express";
 
 // login user
 
 const loginUser = async (req, res) => {
+    const {email, password} = req.body
 
+    try{
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            return res.json({success: false, message: "User does not exist!"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            return res.json({success: false, message: "Invalid Credentials!"})
+        }
+
+        const token = createToken(user._id)
+        res.json({success: true, token})
+
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: "Error"})
+    }
+}
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
 }
 
 // register user
@@ -30,8 +53,19 @@ const registerUser = async (req, res) => {
         // Hash password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-    }catch(error){
 
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save()
+        const token = createToken(user._id)
+        res.json({success: true, token})
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: "Error"})
     }
 }
 
